3d600708269d8c36d813a3d02d328048c2e2b5fc
demidov555 <miha.777vip@mail.ru>
2025-11-28 01:04:28 +0300

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  baseUrl: "project-1ec51f83-51a5-4212-8424-a9804186c040",
+  base: "project-1ec51f83-51a5-4212-8424-a9804186c040",
 });
